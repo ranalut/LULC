c65f94ac220e00a2236cc7e1a2c73c1720de49f1
ranalut <chadwilsey@gmail.com>
2014-03-04 14:40:40 -0800

    update layers.lulc.r

--- layers.lulc.r
+++ layers.lulc.r
@@ -2,21 +2,23 @@ library(raster)
 library(sp)
 
 # Functions
-layers.lulc <- function(file.in, rdata.out, the.crop)
+layers.lulc <- function(file.in, the.crop)
 {
 	# Load LULC data.
 	the.data <- raster(file.in)
 	cat('raster loaded... ')
 	
 	# Crop data
-	r.crop <- raster(the.crop)
-	the.data <- crop(the.data, r.crop)
-	cat('cropped... ')
+	if (is.na(the.crop)==FALSE)
+	{
+		r.crop <- raster(the.crop)
+		the.data <- crop(the.data, r.crop)
+		cat('cropped... ')
+	}
 	
 	# Create a binary raster brick with layerize.
 	the.brick <- layerize(the.data) # This can take a long time.
 	cat('layerized... ')
 
-	save(the.brick, file=rdata.out) # These files are too big to save. Cropped is 10+ GB.
-	cat('written to disc\n')
+	return(the.brick)
 }
