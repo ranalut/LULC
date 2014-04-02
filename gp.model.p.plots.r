18c96190c98ebdcc58b6f7c853349f64b31400e9
ranalut <chadwilsey@gmail.com>
2014-04-02 16:18:31 -0700

    update gp.model.p.plots.r

--- gp.model.p.plots.r
+++ gp.model.p.plots.r
@@ -12,7 +12,7 @@ source('deviance.explained.r')
 # Workspace and parameters
 drive <- 'z'
 workspace <- paste(drive,':/LULC',sep='')
-the.radii <- c(24140,48280) # 48280 # 24140
+the.radii <- c(12070,24140,48280) # 48280 # 24140
 cell.size <- 250
 ag.factors <- c(1,4) # 1 # 4
 
@@ -20,29 +20,29 @@ ag.factors <- c(1,4) # 1 # 4
 # 1 (really none) is default parameters, lr = 0.01, tc = 5
 # 2 is increasing learning rate for most (0.03) , decreasing for some (0.005).
 # 3 is adjusting complexity to keep learning rates < 0.01. 
-ver <- 2
+ver <- 5
 
-spp <- read.csv('z:/lulc/gp_focal_spp_list.csv', stringsAsFactors=FALSE, row.names=1)
+spp <- read.csv(paste('z:/lulc/gp_focal_spp_list_v',ver,'.csv',sep=''), stringsAsFactors=FALSE, row.names=1)
 conus.names <- scan('z:/lulc/legend_conus.txt',what=character(), sep=',')
 conus.names <- c('NA',conus.names[seq(2,34,2)])
 
-var.names <- c(paste('X',c(0,1,2,6:17),sep=''),'hours')
-boxplot.table <- data.frame(conus=c(conus.names[-c(3:5)],'hours'),var=var.names)
+var.names <- c(paste('X',seq(0,17,1),sep=''),'hours')
+boxplot.table <- data.frame(conus=c(conus.names,'hours'),var=var.names)
 
 # for (i in 1:length(spp$BBL_ABBREV)) # max is 22
-# modifier <- 'raptors'; for (i in c(8,9,13,14,16,17,18,22))
-modifier <- 'long.bunt'; for (i in c(2,4,5,10,12,20))
+modifier <- 'raptors'; for (i in c(8,9,13,14,16,17,18,21))
+# modifier <- 'long.bunt'; for (i in c(2,4,5,10,12,20))
 # modifier <- 'game'; for (i in c(1,3,15))
 # modifier <- 'other'; for (i in c(6,7,21,11))
 {
-	for (n in 2) # 1:2)
+	for (n in 1) # 1:2)
 	{
 		for (j in 1) # 1:2)
 		{
 			the.radius <- the.radii[n]
 			ag.factor <- ag.factors[j]
 			species <- spp$BBL_ABBREV[i]
-			if (species=='INBU') { next(j) }
+			# if (species=='INBU') { next(j) }
 			
 			load(paste(workspace,'/Models/gp.lulc.brt.',ver,'.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
 			gbm.plot(brt.model, plot.layout=c(3,3), n.plots=9)
