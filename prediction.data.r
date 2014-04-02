590ffe1a9d5946e0fe10207a3de964e685597f0d
ranalut <chadwilsey@gmail.com>
2014-04-02 08:21:35 -0700

    update prediction.data.r

--- prediction.data.r
+++ prediction.data.r
@@ -1,6 +1,5 @@
 
 # Prediction
-
 library(dismo)
 library(gbm)
 library(tcltk2)
@@ -8,24 +7,16 @@ source('layers.lulc.r')
 source('build.similar.raster.r')
 startTime <- Sys.time()
 
-# source('settings.r')
 # Workspace and parameters
 drive <- 'z'
 workspace <- paste(drive,':/LULC',sep='')
-the.radii <- c(24140,48280) # 48280 # 24140
 cell.size <- 250
-ag.factors <- c(1,4) # 1 # 4
-
-# Versions
-# 1 is default parameters, 2 is increasing learning rate for most, decreasing for some.
-# 3 is adjusting complexity to keep learning rates < 0.01. 
-ver <- 2
-i <- 2005
-the.radius <- the.radii[2]
-ag.factor <- ag.factors[2]
+year <- 2005
+the.radius <- 24140 # 48280
+ag.factor <- 4 # 1 # 4
 
 lulc.data <- layers.lulc(
-			file.in=paste(workspace,'/Historical/conus_historical_y',i,'.img',sep=''),
+			file.in=paste(workspace,'/Historical/conus_historical_y',year,'.img',sep=''),
 			the.crop=paste(workspace,'/gp_backcast_1938_1992/gp_lcyear_1992.tif',sep=''),
 			ag.fact=ag.factor, # NA if no aggregate
 			ag.fun=modal # NA if no aggregate
@@ -46,12 +37,12 @@ for (n in 1:length(pred.data))
 }
 
 pred.data.focal <- brick(pred.data.focal)
-
-the.mask <- raster(paste(workspace,'/gp_backcast_1938_1992/gp_lcyear_1992_',ag.factor*cell.size,'m.tif',sep=''))
-# if (ag.factor!=1) { the.mask <- aggregate(the.mask, fact=ag.factor, fun=modal) }
-pred.data.focal <- mask(x=pred.data.focal, mask=the.mask, maskvalue=0)
+print(pred.data.focal)
+print(names(print.data.focal))
 endTime <- Sys.time()
 print(endTime-startTime)
 # stop('cbw')
 
-names(pred.data.focal) <- c(paste('X',seq(0,16,1),sep=''),'hours')
+# names(pred.data.focal) <- c(paste('X',seq(1,17,1),sep=''),'hours')
+
+writeRaster(pred.data.focal,paste(workspace,'/Predictions/gp.lulc.pred.data.r',the.radius,'m.',ag.factor*cell.size,'m.y',i,'.tif',sep=''), overwrite=TRUE)
