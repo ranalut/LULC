48721e17b16097578c97a57a1ceb7912ccd97943
ranalut <chadwilsey@gmail.com>
2014-04-10 15:59:56 -0700

    update gp.lulc.models.r

--- gp.lulc.models.r
+++ gp.lulc.models.r
@@ -14,8 +14,9 @@ workspace <- paste(drive,':/LULC',sep='')
 the.radii <- c(12070,24140) # 48280 # 24140
 cell.size <- 250
 ag.factors <- c(1,4) # 1 # 4
-ver <- 6
+ver <- 7
 no.backcast <- 'n'
+type <- 'brt'
 
 # Test and training datasets
 n <- 6822 # There are 6822 Records
@@ -25,18 +26,18 @@ train.rows <- drop.test.rows(row.numbers=seq(1,n,1), test.rows=test.rows)
 
 spp <- read.csv(paste('z:/lulc/gp_focal_spp_list_v',ver,'.csv',sep=''), stringsAsFactors=FALSE, row.names=1)
 
-for (i in 1:21) # 1:11 # 12:21 # length(spp$BBL_ABBREV)) # max is 21
+for (n in 1) #1:2
 {
-	for (n in 2) #1:2
+	for (j in 1) #1:2
 	{
-		for (j in 1:2) #1:2
+		for (i in 1:21) # 1:11 # 12:21 # length(spp$BBL_ABBREV)) # max is 21
 		{
 			the.radius <- the.radii[n]
 			ag.factor <- ag.factors[j]
 			species <- spp$BBL_ABBREV[i]
 			if(file.exists(paste(workspace,'/Models/gp.lulc.brt.',ver,'.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))==TRUE) { cat(the.radius,'\n'); next(j) }
 			
-			load(paste(workspace,'/Species/gp.lulc.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
+			load(paste(workspace,'/Species/gp.lulc.',ver,'.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
 
 			the.data <- pa.bird.data[train.rows,]
 			if (no.backcast=='y') { the.data <- the.data[the.data$count_yr >= 92,] }
@@ -46,20 +47,26 @@ for (i in 1:21) # 1:11 # 12:21 # length(spp$BBL_ABBREV)) # max is 21
 			# print(table(the.data$how_many))
 			
 			# RF
-			the.data$detect <- factor(the.data$detect, levels=c(0,1))
-			rf.model <- randomForest(y=the.data$detect, x=the.data[,c(7,11:27)] , importance=TRUE)
-			# print(rf.model)
-			# print(rf.model$importance)
-			save(rf.model,file=paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.rf.',ver,'.',species,'.r',the.radius,'m.', ag.factor*cell.size,'m.rdata',sep=''))
-			
+			if (type=='rf')
+			{
+				the.data$detect <- factor(the.data$detect, levels=c(0,1))
+				rf.model <- randomForest(y=the.data$detect, x=the.data[,c(7,11:27)] , importance=TRUE, ntree=500)
+				# print(rf.model)
+				# print(rf.model$importance)
+				save(rf.model,file=paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.rf.',ver,'.',species,'.r', the.radius,'m.', ag.factor*cell.size,'m.rdata',sep=''))
+			}
 			# BRT
-			# brt.model <- gbm.step(data=the.data, gbm.x=c(7,12:28), gbm.y=9, family="binomial", tree.complexity=spp$complexity[i], learning.rate=spp$learning.rate[i], bag.fraction=0.5, verbose=FALSE)
-			# cat('# trees = ',brt.model$n.trees,'\n')
-			# save(brt.model,file=paste(workspace,'/Models/GreatPlains/Abundance/gp.lulc.brt.',ver,'.',species,'.r',the.radius,'m.', ag.factor*cell.size,'m.rdata',sep=''))
-					
+			if (type=='brt')
+			{
+				brt.model <- gbm.step(data=the.data, gbm.x=c(7,11:27), gbm.y=10, family="bernoulli", tree.complexity=spp$complexity[i], learning.rate=spp$learning.rate[i], bag.fraction=0.5, verbose=FALSE)
+				cat('# trees = ',brt.model$n.trees,'\n')
+				save(brt.model,file=paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.brt.',ver,'.',species,'.r',the.radius,'m.', ag.factor*cell.size,'m.rdata',sep=''))
+			}
 			cat('\nend model',species,'############################\n')
 			# stop('cbw')
 		}
+	# stop('cbw')
 	}
 }
+
 
