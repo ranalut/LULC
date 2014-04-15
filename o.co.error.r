4acdacb99e9b2ba514afce7fe6b45df5409e4f93
ranalut <chadwilsey@gmail.com>
2014-04-15 15:58:24 -0700

    update o.co.error.r

--- o.co.error.r
+++ o.co.error.r
@@ -3,6 +3,7 @@
 
 o.co.error <- function(mat)
 {
+	if(sum(dim(mat))!=4) { return(list(NA,NA)) }
 	omission <- round(100*mat[1,2]/sum(mat[,2]))
 	commission <- round(100*mat[2,1]/sum(mat[,1]))
 	return(list(omission,commission))
