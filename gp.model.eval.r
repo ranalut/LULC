c9f2bd5323ffe5a723329ba8390e1a4222d6042c
ranalut <chadwilsey@gmail.com>
2014-04-15 15:57:50 -0700

    update gp.model.eval.r

--- gp.model.eval.r
+++ gp.model.eval.r
@@ -22,8 +22,8 @@ ag.factors <- c(1,4) # 1 # 4
 # 1 (really none) is default parameters, lr = 0.01, tc = 5
 # 2 is increasing learning rate for most (0.03) , decreasing for some (0.005).
 # 3 is adjusting complexity to keep learning rates < 0.01. 
-ver <- 6
-no.backcast <- 'n'
+ver <- 8
+no.backcast <- 'y'
 type <- 'brt'
 
 # Test and training datasets
@@ -35,12 +35,12 @@ spp <- read.csv(paste('z:/lulc/gp_focal_spp_list_v',ver,'.csv',sep=''), stringsA
 
 for (n in 1) # 1:3
 {
-	for (j in 2)
+	for (j in 1:2)
 	{
-		output <- as.data.frame(matrix(rep(NA,dim(spp)[1]*6),ncol=6))
-		colnames(output) <- c('auc','cutoff','train.om','train.co','test.om','test.co')
+		output <- as.data.frame(matrix(rep(NA,dim(spp)[1]*8),ncol=8))
+		colnames(output) <- c('auc','kappa','cutoff','train.om','train.co','test.om','test.co','dev.exp.test')
 
-		for (i in 1:length(spp$BBL_ABBREV)) # c(1,2,5:21) # 1:length(spp$BBL_ABBREV) # max is 22
+		for (i in 3) # 1:length(spp$BBL_ABBREV) # c(1,2,5:21)
 		{
 			the.radius <- the.radii[n]
 			ag.factor <- ag.factors[j]
@@ -48,28 +48,24 @@ for (n in 1) # 1:3
 			
 			if (file.exists(paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.',type,'.',ver,'.',species,'.r',the.radius,'m.', ag.factor*cell.size,'m.rdata',sep=''))==FALSE) { cat('missing... ',species,' ',the.radius,' ',ag.factor*cell.size,'\n'); next(j) }
 			
-			load(paste(workspace,'/Species/gp.lulc.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
+			load(paste(workspace,'/Species/gp.lulc.',ver,'.',species,'.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
 			load(paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.',type,'.',ver,'.',species,'.r',the.radius,'m.',ag.factor*cell.size, 'm.rdata',sep=''))
-			cat('loaded... ',species,' ',the.radius,' ',ag.factor*cell.size,'\n')
+			cat('\nloaded... ',species,' ',the.radius,' ',ag.factor*cell.size,'\n')
 			
 			if (no.backcast=='y')
 			{
-				# evaluation <- model.eval.binary(the.model=rf.model, covariates=pa.bird.data[train.rows,], test.cov=pa.bird.data[test.rows,])
-				# covariates <- pa.bird.data[test.rows,]
-				# covariates <- covariates[covariates$count_yr >= 92,c(7,11:27)]
-				# obs <- pa.bird.data[test.rows,]
-				# obs <- obs[obs$count_yr >= 92,9]
-				# evaluation <- model.eval.abund(the.model=brt.model, covariates=covariates, obs=obs, spp=species)
+				temp <- as.integer(rownames(pa.bird.data))
+				if (type=='brt') { evaluation <- model.eval.binary(the.model=brt.model, covariates=pa.bird.data[temp%in%train.rows & pa.bird.data$count_yr >= 92,c(7,10:27)], test.cov=pa.bird.data[temp%in%test.rows & pa.bird.data$count_yr >= 92,c(7,10:27)], n.trees=brt.model$n.trees) }
 			}
 			else
 			{
 				if (type=='rf') { evaluation <- model.eval.binary(the.model=rf.model, covariates=pa.bird.data[train.rows,c(7,10:27)], test.cov=pa.bird.data[test.rows,c(7,10:27)]) }
 				if (type=='brt') { evaluation <- model.eval.binary(the.model=brt.model, covariates=pa.bird.data[train.rows,c(7,10:27)], test.cov=pa.bird.data[test.rows,c(7,10:27)], n.trees=brt.model$n.trees) }
 			}
-			
+			stop('cbw')
 			save(evaluation,file=paste(workspace,'/Models/GreatPlains/Distribution/gp.lulc.',type,'.eval.',ver,'.',species, '.r',the.radius,'m.',ag.factor*cell.size,'m.rdata',sep=''))
 			
-			output[i,] <- as.numeric(c(evaluation[[1]],evaluation[[2]],o.co.error(evaluation[[3]]),o.co.error(evaluation[[4]])))
+			output[i,] <- as.numeric(c(evaluation[[1]],evaluation[[2]],evaluation[[3]],o.co.error(evaluation[[4]]),o.co.error(evaluation[[5]]),evaluation[[6]]))
 			
 			# cat('\nend nass',species,'############################\n')
 			# stop('cbw')
