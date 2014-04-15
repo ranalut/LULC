f8fff31e889ca28171829c89316a8673a418dcc7
ranalut <chadwilsey@gmail.com>
2014-04-15 15:58:29 -0700

    update versions.r

--- versions.r
+++ versions.r
@@ -5,5 +5,5 @@
 # 5. Included only data from 1992-2005.  BRT models of abundance. 12km 24km 48km, 250m 1000m 
 # 6. Included all data.  BRT and RF models of presence/absence. 12km 24km, 250m 1000m
 # 7. Included all data, but matched only to LULC data for 2005.  Test whether historical data matters. 12km, 250 & 1000m
-
+# 8. Included only data from 1992-2005.  BRT models of presence/absence.  12km, 250 & 1000m
 
