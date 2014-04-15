7952eeebd5d944721e198a4e1f794b118403c33f
ranalut <chadwilsey@gmail.com>
2014-04-15 15:58:17 -0700

    update model.eval.fxn.r

--- model.eval.fxn.r
+++ model.eval.fxn.r
@@ -1,6 +1,7 @@
 
 # Model evaluation
 library(PresenceAbsence)
+library(dismo)
 
 model.eval.abund <- function(the.model, covariates, obs, spp)
 # model.eval <- function(the.model=nass.models[[i]], covariates=nass.spp.data[[i]][,c(4:5,9:26)], test.rows=test.rows, obs=nass.spp.data[[i]][,6], spp=spp.names[i])
@@ -41,7 +42,7 @@ model.eval.binary <- function(the.model, covariates, test.cov, n.trees=NA)
 	thresh.table <- data.frame(sites=seq(1,dim(covariates)[1],1),obs=covariates[,c('detect')],pred=temp)
 	# print(head(thresh.table)); print(table(thresh.table$obs))
 	auc <- auc(thresh.table) 
-	print(auc)
+	cat('AUC... ',as.numeric(auc[1]),'\n')
 	auc.roc.plot(thresh.table)
 	thresh.optim <- optimal.thresholds(DATA=thresh.table, opt.methods=c('Sens=Spec','MaxKappa','ReqSpec'),req.spec=1)
 	# print(thresh.optim)
@@ -51,17 +52,23 @@ model.eval.binary <- function(the.model, covariates, test.cov, n.trees=NA)
 	conf.matrix.train <- table(thresh.table[,2:3])
 	
 	# Test Data
-	if (is.na(n.trees)==FALSE) { test.pred <- predict(the.model, newdata=test.cov, type='response', n.trees=n.trees) }
-	else { test.pred <- predict(the.model, newdata=test.cov, type='prob') }
+	if (is.na(n.trees)==FALSE) { pred.prob <- predict(the.model, newdata=test.cov, type='response', n.trees=n.trees) }
+	else { pred.prob <- predict(the.model, newdata=test.cov, type='prob') }
+	if (is.vector(pred.prob)==FALSE) { pred.prob <- as.numeric(pred.prob[,2]) }
 	
-	if (is.vector(test.pred)==TRUE)	{ temp <- test.pred }
-	else { temp <- as.numeric(test.pred[,2]) }
-	test.pred <- data.frame(data=temp)
-	
-	test.pred$pred <- ifelse(test.pred$data >= cutoff,1,0)
+	test.pred <- data.frame(sites=seq(1,dim(test.cov)[1]))
 	test.pred$obs <- as.numeric(test.cov[,'detect'])
+	test.pred$pred <- ifelse(pred.prob >= cutoff,1,0)
 	conf.matrix.test <- table(test.pred[,c('pred','obs')])
-	print(conf.matrix.test)
-
-	return(list(round(auc[1],3),cutoff,conf.matrix.train, conf.matrix.test))
+	# print(conf.matrix.test)
+	
+	kap <- Kappa(cmx(test.pred,threshold=cutoff),st.dev=FALSE)
+	cat('Kappa... ',kap,'\n')
+	
+	dev.exp.test <- dsq(
+		mean.null=calc.deviance(as.numeric(test.cov$detect), rep(mean(as.numeric(test.cov$detect)),dim(test.cov)[1]), family="bernoulli"),
+		validation=calc.deviance(as.numeric(test.cov$detect), pred.prob, family="bernoulli")
+		)
+	cat('deviance explained... ',dev.exp.test,'\n')
+	return(list(round(auc[1],3),round(kap,3), cutoff, conf.matrix.train, conf.matrix.test, dev.exp.test))
 }
