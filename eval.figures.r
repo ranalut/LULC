64c0f398af8271103ebfc766af1dda981a79721c
ranalut <chadwilsey@gmail.com>
2014-04-16 14:28:07 -0700

    update eval.figures.r

--- eval.figures.r
+++ eval.figures.r
@@ -1,25 +1,28 @@
 
 # Workspace and parameters
 drive <- 'z'
-workspace <- paste(drive,':/LULC',sep='')
-the.radii <- c(12070,24140)
-cell.size <- 250
-ag.factors <- c(1,4)
-no.backcast <- 'n'
-type <- 'brt'
+# workspace <- paste(drive,':/LULC',sep='')
+# the.radii <- c(12070,24140)
+# cell.size <- 250
+# ag.factors <- c(1,4)
+# no.backcast <- 'n'
+# type <- 'brt'
 
-comparison <- 'radius' # 'resolution' # 'radius'
-comp.var <- c('12km','24km') # c('12km','24km')
-data.1 <- read.csv(paste(drive,':/LULC/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.brt.eval.6.r12070m.250m.csv',sep=''),header=TRUE,row.names=1)
+comparison <- 'temporal.3' #'temporal' # 'temporal.2' # 'temporal' # 'resolution' # 'radius'
+# comp.var <- c('12070m','24140m') # c('12km','24km')
+# comp.var <- c('1966-2005, annual matching LULC','1966-2005, only 2005 LULC')
+# comp.var <- c('1966-2005, annual matching LULC','1992-2005, annual matching LULC')
+comp.var <- c('1966-2005, only 2005 LULC','1992-2005, annual matching LULC')
+data.1 <- read.csv(paste(drive,':/LULC/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.brt.eval.7.r12070m.250m.csv',sep=''),header=TRUE,row.names=1)
 data.1[,5:8] <- data.1[,5:8]/100
-data.2 <- read.csv(paste(drive,':/LULC/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.brt.eval.6.r24140m.250m.csv',sep=''),header=TRUE,row.names=1)
+data.2 <- read.csv(paste(drive,':/LULC/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.brt.eval.8.r12070m.250m.csv',sep=''),header=TRUE,row.names=1)
 data.2[,5:8] <- data.2[,5:8]/100
 
 variables <- c('kappa','dev.exp.test','test.om','test.co')
 var.names <- c('KAPPA','DEVIANCE EXPLAINED','OMISSION ERROR','COMMISSION ERROR')
 
-png(paste(workspace,'/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.fig.',comparison,'.png',sep=''),width=900,height=600,pointsize=14)
-	par(mfrow=c(3,1))
+png(paste(workspace,'/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.fig.',comparison,'.png',sep=''),width=900,height=900,pointsize=14)
+	par(mfrow=c(4,1))
 	
 	for (i in 1:length(variables))
 	{
@@ -74,17 +77,3 @@ png(paste(workspace,'/Models/GreatPlains/Distribution/EvaluationTables/gp.lulc.x
 	}
 dev.off()	
 	
-# col.names <- c('auc','cutoff','train.omission','train.commission','test.omission','test.commission')
-
-
-# temp <- t(output[,2:dim(output)[2]])
-# temp[3:6,] <- temp[3:6,]/100
-# # output <- data.frame(spp[,c(1:3,6)],output)
-# # colnames(output) <- c(colnames(output)[1:4],col.names)
-# # write.csv(output, paste('z:/lulc/dev.exp.test.v',ver,'.csv',sep=''))
-
-# colnames(temp) <- output$species
-# barplot(temp, ylim=c(0,1), legend.text=col.names, args.legend=list(x=1,y=1,bty='n', horiz=TRUE), beside=TRUE)
-
-
-
