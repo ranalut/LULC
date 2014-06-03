6332b5608faad152d1f28726de72ba2d18ef1668
ranalut <chadwilsey@gmail.com>
2014-06-03 15:19:37 -0700

    update versions.r

--- versions.r
+++ versions.r
@@ -1,9 +1,13 @@
 
-# Versions
+# Versions LULC models
 
 # 4. Included all data.  BRT models of abundance. 24km 48km, 250m 1000m
 # 5. Included only data from 1992-2005.  BRT models of abundance. 12km 24km 48km, 250m 1000m 
 # 6. Included all data.  BRT and RF models of presence/absence. 12km 24km, 250m 1000m
 # 7. Included all data, but matched only to LULC data for 2005.  Test whether historical data matters. 12km, 250 & 1000m
 # 8. Included only data from 1992-2005.  BRT models of presence/absence.  12km, 250 & 1000m
+
+# Bioclimatic Models
+# 8. Included all data.  BRT models of presence/absence.  Directly sampled climate from climate grid
+# 8.92.  Included only data from 1992-2005.  BRT model of presence/absence.  
 
