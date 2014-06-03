92fac4b29d3ecf4ae0dd357d10b036ce6dcb4b30
ranalut <chadwilsey@gmail.com>
2014-06-03 15:57:50 -0700

    create lm.and.offset.model.r

--- lm.and.offset.model.r
+++ lm.and.offset.model.r
@@ -1,8 +1,8 @@
 
 lm.and.offset.models <- function(the.data)
 {
-	lm.model <- lm(the.data[,2] ~ the.data[,1])
-	offset.model <- lm(the.data[,2] ~ the.data[,1] + offset(the.data[,1])
+	lm.model <- lm(the.data[,2] ~ the.data[,1] - 1)
+	offset.model <- lm(the.data[,2] ~ the.data[,1] + offset(the.data[,1]) - 1)
 	
 	return(list(lm.model,offset.model))
 }
