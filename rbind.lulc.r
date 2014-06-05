4f48f613ec3f3bc0ee6ba5b3d8829f8d566bb6ab
ranalut <chadwilsey@gmail.com>
2014-06-05 16:22:23 -0700

    update rbind.lulc.r

--- rbind.lulc.r
+++ rbind.lulc.r
@@ -7,18 +7,21 @@ rbind.lulc <- function(lulc.list, years, pts)
 	the.data <- lulc.list
 	n <- dim(the.data[[1]])[1]
 	
+	# Add missing columns for missing classes.
 	for (i in 1:length(the.data))
 	{
 		the.data[[i]] <- add.col(in.data=the.data[[i]],ncols=seq(1,17,1))
 	}
 	# stop('cbw')
 	
+	# Add columns for CBC circle abbreviation and count year.
 	for (i in 1:length(the.names))
 	{
 		the.data[[the.names[i]]] <- data.frame(abbrev=pts$abbrev, count_yr=rep((years[i]-1900),n),the.data[[the.names[i]]])
 	}
 	# return(the.data)
 	
+	# Convert list to large dataframe.
 	output <- the.data[[the.names[1]]]
 	# cat(dim(output)[1],' ')
 	for (i in 2:length(the.names))
