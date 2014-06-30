3b132992630604b7b2c3d0bb083e6410b7caee45
ranalut <chadwilsey@gmail.com>
2014-06-30 08:09:42 -0700

    update strongholds.zonal.r

--- strongholds.zonal.r
+++ strongholds.zonal.r
@@ -1,7 +1,9 @@
 library(raster)
-create.zones <- 'y'
+create.zones <-	'n'
+do.present <- 	'n'
+do.future <- 	'y'
 
-if (create.zones=='n')
+if (create.zones=='y')
 {
 	# Preparation.  Create a strongholds cell index grid to define zones.
 	# load strongholds grid
@@ -40,20 +42,52 @@ if (create.zones=='n')
 # Reprojected strongholds grid
 zones <- raster('d:/strongholds/zones_raster2.tif')
 
-# LULC data
-lulc.brick <- brick('d:/lulc/historical/conus.lulc.brick.r250m.y2005.tif')
+# Present
+if(do.present=='y')
+{
+	# LULC data
+	lulc.brick <- brick('d:/lulc/historical/conus.lulc.brick.r250m.y2005.tif')
+	lookup.table <- read.csv('d:/strongholds/lookup.table.csv', header=TRUE, row.names=1)
+	
+	startTime <- Sys.time()
+	zonal.mean.mat <- zonal(lulc.brick, zones, fun='mean', digits=4, na.rm=TRUE) # This took 4.45 hours.
+	write.csv(zonal.mean.mat, 'd:/strongholds/lulc.strong.2005.csv')
+	print(Sys.time() - startTime)
 
-startTime <- Sys.time()
-zonal.mean.mat <- zonal(lulc.brick, zones, fun='mean', digits=4, na.rm=TRUE) # This took 4.45 hours.
-write.csv(zonal.mean.mat, 'd:/strongholds/lulc.strong.2005.csv')
-print(Sys.time() - startTime)
+	output <- merge(lookup.table, zonal.mean.mat, all.x=TRUE)
+	output <- output[order(output$strong),]
+	output$score <- as.vector(as.matrix(strong))
+	write.csv(output, 'd:/strongholds/lulc.strong.2005.full.csv')
+}
 
-output <- merge(lookup.table, zonal.mean.mat, all.x=TRUE)
-output <- output[order(output$strong),]
-output$score <- as.vector(as.matrix(strong))
-write.csv(output, 'd:/strongholds/lulc.strong.2005.full.csv')
+# Future
+if (do.future=='y')
+{
+	drive <- 'd'
+	scenarios <- c('a1b','a2','b2')
+	years <- c(2020,2050,2080)
+	paths1 <- paste(drive,':/LULC/conus_',scenarios,'/conus_',scenarios,sep='')
+	paths2 <- paste(rep(paths1,each=3),'_y',years,sep='')
+	paths3 <- paste(paths2,'.tif',sep='')
 
+	# for (i in 1:length(paths3))
+	for (i in c(6,9,2,5,8,1,4,7)) # 3,
+	{
+		lulc.brick <- brick(paths3[i])
+		lookup.table <- read.csv('d:/strongholds/lookup.table.csv', header=TRUE, row.names=1)
+	
+		startTime <- Sys.time()
+		zonal.mean.mat <- zonal(lulc.brick, zones, fun='mean', digits=4, na.rm=TRUE) # This took 4.45 hours.
+		write.csv(zonal.mean.mat, paste('d:/strongholds/lulc.strong.',rep(scenarios,each=3)[i],'.y',rep(years,3)[i],'.csv',sep=''))
+		print(Sys.time() - startTime)
 
-
+		output <- merge(lookup.table, zonal.mean.mat, all.x=TRUE)
+		output <- output[order(output$strong),]
+		output$score <- as.vector(as.matrix(strong))
+		write.csv(output, paste('d:/strongholds/lulc.strong.',rep(scenarios,each=3)[i],'.y',rep(years,3)[i],'.full.csv',sep=''))
+	
+		file.remove(dir('C:/Users/cwilsey/AppData/Local/Temp/R_raster_cwilsey/',full.names=TRUE))
+	}
+}
 stop('cbw')
 
